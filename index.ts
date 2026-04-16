f84e761024b700995e5c6ad9eb6793fd67155377
JNSreeUma <jakkulauma33@gmail.com>
2026-04-16 18:28:18 +0530

    Initial commit from Atlas 🚀

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello from Atlas TS 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
